7c23f47cdda71b1a3528432c186454485fb235a0
项目维护者 <project-maintainer@local.invalid>
2026-09-23 10:02:20 +0800

    初始化缺陷复现项目

---
+++ beetsplug/importadded.py
@@ -0,0 +1,175 @@
+""" . "说明"Populate an item's `added` and `mtime` fields by using the file
+modification time (mtime) of the item's source file before import.
+
+Reimported albums and items are skipped.
+""" . "说明"
+
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING
+
+from beets import importer, util
+from beets.plugins import BeetsPlugin
+
+if TYPE_CHECKING:
+    from beets.importer import ImportSession, ImportTask
+    from beets.library import Album, Item, Library
+
+
+class ImportAddedPlugin(BeetsPlugin):
+    item_mtime: dict[bytes, float]
+    reimported_item_ids: set[int | None]
+    replaced_album_paths: set[bytes]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.config.add(
+            {"preserve_mtimes": False, "preserve_write_mtimes": False}
+        )
+
+        # item.id for new items that were reimported
+        self.reimported_item_ids = set()
+        # album.path for old albums that were replaced by a reimported album
+        self.replaced_album_paths = set()
+        # item path in the library to the mtime of the source file
+        self.item_mtime = {}
+
+        register = self.register_listener
+        register("import_task_created", self.check_config)
+        register("import_task_created", self.record_if_inplace)
+        register("import_task_files", self.record_reimported)
+        register("before_item_moved", self.record_import_mtime)
+        register("item_copied", self.record_import_mtime)
+        register("item_linked", self.record_import_mtime)
+        register("item_hardlinked", self.record_import_mtime)
+        register("item_reflinked", self.record_import_mtime)
+        register("album_imported", self.update_album_times)
+        register("item_imported", self.update_item_times)
+        register("after_write", self.update_after_write_time)
+
+    def check_config(
+        self, task: ImportTask, session: ImportSession
+    ) -> list[ImportTask] | None:
+        self.config["preserve_mtimes"].get(bool)
+        return None
+
+    def reimported_item(self, item: Item) -> bool:
+        return item.id in self.reimported_item_ids
+
+    def reimported_album(self, album: Album) -> bool:
+        return album.path in self.replaced_album_paths
+
+    def record_if_inplace(
+        self, task: ImportTask, session: ImportSession
+    ) -> list[ImportTask] | None:
+        if not (
+            session.config["copy"]
+            or session.config["move"]
+            or session.config["link"]
+            or session.config["hardlink"]
+            or session.config["reflink"]
+        ):
+            self._log.debug(
+                "In place import detected, recording mtimes from source paths"
+            )
+            items = (
+                [task.item]
+                if isinstance(task, importer.SingletonImportTask)
+                else task.items
+            )
+            for item in items:
+                self.record_import_mtime(item, item.path, item.path)
+
+        return None
+
+    def record_reimported(
+        self, task: ImportTask, session: ImportSession
+    ) -> None:
+        self.reimported_item_ids = {
+            item.id
+            for item, replaced_items in task.replaced_items.items()
+            if replaced_items
+        }
+        self.replaced_album_paths = set(task.replaced_albums.keys())
+
+    def write_file_mtime(self, path: str, mtime: float) -> None:
+        """ . "说明"Write the given mtime to the destination path.""" . "说明"
+        stat = os.stat(util.syspath(path))
+        os.utime(util.syspath(path), (stat.st_atime, mtime))
+
+    def write_item_mtime(self, item: Item, mtime: float) -> None:
+        """ . "说明"Write the given mtime to an item's `mtime` field and to the mtime
+        of the item's file.
+        """ . "说明"
+        # The file's mtime on disk must be in sync with the item's mtime
+        self.write_file_mtime(util.syspath(item.path), mtime)
+        item.mtime = int(mtime)
+
+    def record_import_mtime(
+        self, item: Item, source: bytes, destination: bytes
+    ) -> None:
+        """ . "说明"Record the file mtime of an item's path before its import.""" . "说明"
+        mtime = os.stat(util.syspath(source)).st_mtime
+        self.item_mtime[destination] = mtime
+        self._log.debug(
+            "Recorded mtime {} for item '{}' imported from '{}'",
+            mtime,
+            util.displayable_path(destination),
+            util.displayable_path(source),
+        )
+
+    def update_album_times(self, lib: Library, album: Album) -> None:
+        if self.reimported_album(album):
+            self._log.debug(
+                "Album '{.filepath}' is reimported, skipping import of "
+                "added dates for the album and its items.",
+                album,
+            )
+            return
+
+        album_mtimes = []
+        for item in album.items():
+            mtime = self.item_mtime.pop(item.path, None)
+            if mtime:
+                album_mtimes.append(mtime)
+                if self.config["preserve_mtimes"].get(bool):
+                    self.write_item_mtime(item, mtime)
+                    item.store()
+        album.added = min(album_mtimes)
+        self._log.debug(
+            "Import of album '{0.album}', selected album.added={0.added} "
+            "from item file mtimes.",
+            album,
+        )
+        album.store()
+
+    def update_item_times(self, lib: Library, item: Item) -> None:
+        if self.reimported_item(item):
+            self._log.debug(
+                "Item '{.filepath}' is reimported, skipping import of added date.",
+                item,
+            )
+            return
+        mtime = self.item_mtime.pop(item.path, None)
+        if mtime:
+            item.added = mtime
+            if self.config["preserve_mtimes"].get(bool):
+                self.write_item_mtime(item, mtime)
+            self._log.debug(
+                "Import of item '{0.filepath}', selected item.added={0.added}",
+                item,
+            )
+            item.store()
+
+    def update_after_write_time(self, item: Item, path: bytes) -> None:
+        """ . "说明"Update the mtime of the item's file with the item.added value
+        after each write of the item if `preserve_write_mtimes` is enabled.
+        """ . "说明"
+        if item.added:
+            if self.config["preserve_write_mtimes"].get(bool):
+                self.write_item_mtime(item, item.added)
+            self._log.debug(
+                "Write of item '{0.filepath}', selected item.added={0.added}",
+                item,
+            )
---
+++ test/plugins/test_importadded.py
@@ -0,0 +1,141 @@
+""" . "说明"Tests for the `importadded` plugin.""" . "说明"
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pytest
+
+from beets import importer
+from beets.test.helper import AutotagImportTestCase, PluginMixin
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+
+def modify_mtimes(paths: Iterable[Path], offset=-60000):
+    for i, path in enumerate(paths, start=1):
+        mstat = path.stat()
+        os.utime(path, (mstat.st_atime, mstat.st_mtime + offset * i))
+
+
+class ImportAddedTest(PluginMixin, AutotagImportTestCase):
+    # The minimum mtime of the files to be imported
+    plugin = "importadded"
+    min_mtime = None
+
+    def setUp(self):
+        super().setUp()
+        self.prepare_album_for_import(2)
+        # Different mtimes on the files to be imported in order to test the
+        # plugin
+        paths = [Path(mfile.path) for mfile in self.import_media]
+        modify_mtimes(paths)
+        self.min_mtime = min(p.stat().st_mtime for p in paths)
+        self.importer = self.setup_importer()
+        self.importer.add_choice(importer.Action.APPLY)
+
+    def find_media_file_mtime(self, item) -> float:
+        """ . "说明"Find the pre-import MediaFile for an Item""" . "说明"
+        for m in self.import_media:
+            if m.title.replace("Tag", "Applied") == item.title:
+                return Path(m.path).stat().st_mtime
+        raise AssertionError(f"No MediaFile found for Item {item.filepath}")
+
+    def test_import_album_with_added_dates(self):
+        self.importer.run()
+
+        album = self.lib.albums().get()
+        assert album.added == self.min_mtime
+        for item in album.items():
+            assert item.added == self.min_mtime
+
+    def test_import_album_inplace_with_added_dates(self):
+        self.config["import"]["copy"] = False
+
+        self.importer.run()
+
+        album = self.lib.albums().get()
+        assert album.added == self.min_mtime
+        for item in album.items():
+            assert item.added == self.min_mtime
+
+    def test_import_album_with_preserved_mtimes(self):
+        self.config["importadded"]["preserve_mtimes"] = True
+        self.importer.run()
+        album = self.lib.albums().get()
+        assert album.added == self.min_mtime
+        for item in album.items():
+            assert item.added == pytest.approx(self.min_mtime, rel=1e-4)
+            mediafile_mtime = self.find_media_file_mtime(item)
+            assert item.mtime == pytest.approx(mediafile_mtime, rel=1e-4)
+            assert item.filepath.stat().st_mtime == pytest.approx(
+                mediafile_mtime, rel=1e-4
+            )
+
+    def test_reimported_album_skipped(self):
+        # Import and record the original added dates
+        self.importer.run()
+        album = self.lib.albums().get()
+        album_added_before = album.added
+        items_added_before = {
+            item.filepath: item.added for item in album.items()
+        }
+        # Newer Item path mtimes as if Beets had modified them
+        modify_mtimes(items_added_before.keys(), offset=10000)
+        # Reimport
+        self.setup_importer(import_dir=self.lib_path)
+        self.importer.run()
+        # Verify the reimported items
+        album = self.lib.albums().get()
+        assert album.added == pytest.approx(album_added_before, rel=1e-4)
+        items_added_after = {
+            item.filepath: item.added for item in album.items()
+        }
+        for item_path, added_after in items_added_after.items():
+            assert items_added_before[item_path] == pytest.approx(
+                added_after, rel=1e-4
+            ), f"reimport modified Item.added for {item_path}"
+
+    def test_import_singletons_with_added_dates(self):
+        self.config["import"]["singletons"] = True
+        self.importer.run()
+        for item in self.lib.items():
+            assert item.added == pytest.approx(
+                self.find_media_file_mtime(item), rel=1e-4
+            )
+
+    def test_import_singletons_with_preserved_mtimes(self):
+        self.config["import"]["singletons"] = True
+        self.config["importadded"]["preserve_mtimes"] = True
+        self.importer.run()
+        for item in self.lib.items():
+            mediafile_mtime = self.find_media_file_mtime(item)
+            assert item.added == pytest.approx(mediafile_mtime, rel=1e-4)
+            assert item.mtime == pytest.approx(mediafile_mtime, rel=1e-4)
+            assert item.filepath.stat().st_mtime == pytest.approx(
+                mediafile_mtime, rel=1e-4
+            )
+
+    def test_reimported_singletons_skipped(self):
+        self.config["import"]["singletons"] = True
+        # Import and record the original added dates
+        self.importer.run()
+        items_added_before = {
+            item.filepath: item.added for item in self.lib.items()
+        }
+        # Newer Item path mtimes as if Beets had modified them
+        modify_mtimes(items_added_before.keys(), offset=10000)
+        # Reimport
+        self.setup_importer(import_dir=self.lib_path, singletons=True)
+        self.importer.run()
+        # Verify the reimported items
+        items_added_after = {
+            item.filepath: item.added for item in self.lib.items()
+        }
+        for item_path, added_after in items_added_after.items():
+            assert items_added_before[item_path] == pytest.approx(
+                added_after, rel=1e-4
+            ), f"reimport modified Item.added for {item_path}"
